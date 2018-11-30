6c270795cd4c334071bf3b8338969907b53dd08c
Elineda <henecro@yahoo.fr>
2018-11-30 09:31:14 +0100

    bbou

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Design Tech Académie</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <img src="tuturu.png"></img>
+    </header>
+    <main>
+
+      <?php
+      echo "coucou";
+      ?>
+</main>
+    <footer>
+
+    </footer>
+
+
+
+<script src="script.js" type="text/javascript"></script>
+  </body>
+</html>
