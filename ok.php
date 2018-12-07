ae55a4136e5e0c8a1bf9a52aa8c962368d5db94d
Elineda <henecro@yahoo.fr>
2018-12-07 16:36:49 +0100

    p

--- ok.php
+++ ok.php
@@ -2,9 +2,19 @@
 
 
   $ecrit = file_get_contents('php://input');
-  $fichier=fopen("data.json","w");
-  fwrite($fichier,$ecrit);
-  fclose($fichier);
+  $decode=json_decode($ecrit);
+  if (! $decode) {
+    http_response_code(415);
+  }
+  elseif (! $decode->temperature || ! $decode->humidite) {
+    http_response_code(400);
+  }
+  else {
+    $fichier=fopen("data.json","w");
+    fwrite($fichier,$ecrit);
+    fclose($fichier);
+  }
+
 
 
 
