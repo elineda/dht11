c801020ecf7f10a708d5b397f369adea21b6df9b
Elineda <henecro@yahoo.fr>
2018-12-07 14:01:27 +0100

    creation page d'envoie

---
+++ entre.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Termotuturu</title>
+    <link rel="stylesheet" href="main.css" />
+  </head>
+  <body>
+    <header>
+      <img src="tuturu.png"></img>
+    </header>
+    <main>
+      <h1>Envoie</h1>
+<form class="entre" action="ok.php" method="post">
+  <p>Temperature : </p>
+  <input type="number" name="temperature" value="0" id="temperature">
+  <button type="submit" name="button">goooooooooo</button>
+</form>
+</main>
+    <footer>
+
+    </footer>
+
+
+
+<script src="script.js" type="text/javascript"></script>
+  </body>
+</html>
